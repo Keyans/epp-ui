7c9fc17a9397e141acb847a737cc8bebd4d19f0a
abbotwu <abbotwu@tencent.com>
2020-09-04 23:54:10 +0800

    创建脚手架

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,51 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-08-26 16:33:33
+ * @LastEditTime: 2020-09-04 20:08:35
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /coco_tob_cms/Users/abbotwu/project/errordemo/src/components/HelloWorld.vue
+-->
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div class="hello"></div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      setTimeout(() => {
+        this.test();
+      });
+      // window.onerror=(...args)=>{
+      //   console.log(args, 222);
+      // }
+      window.addEventListener("unhandledrejection",e=>{
+        throw e.reason
+      })
+      window.addEventListener("error",args=>{
+              console.log(args, 555);
+              return true;
+      },true)
+      setTimeout(() => {
+        console.log("2=>start");
+        console.log("2=>end");
+      });
+    },
+    test() {
+      console.log("1=>start");
+      error;
+      console.log("1=>end");
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-26 16:33:33
+ * @LastEditTime: 2020-09-04 20:23:51
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /demo/Users/abbotwu/project/errordemo/src/main.js
+ */
 import Vue from 'vue'
 import App from './App.vue'
 
